0d01bbc779b4cb6dac74fef4109c5b9c0fe0f148
shivamrishi98 <shivamrishisr@gmail.com>
2020-04-01 13:48:45 +0530

    Interview Jumpstart questions : Leaders In Array done

---
+++ src/com/company/InterviewJumpStart/LeadersInArray.java
@@ -0,0 +1,41 @@
+package com.company.InterviewJumpStart;
+
+import java.util.Scanner;
+
+public class LeadersInArray {
+
+    
+    public static void leaders(int[] input) {
+
+        int size=input.length;
+        for(int i=0;i<size;i++)
+        {
+            int j;
+            for(j=i+1;j<size;j++)
+            {
+                if(input[i]<input[j])
+                {
+                    break;
+                }
+
+
+            }
+            if(j==size)
+            {
+                System.out.print(input[i]+" ");
+            }
+
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int input[] = new int[n];
+        for(int i = 0; i < n; i++){
+            input[i] = s.nextInt();
+        }
+        leaders(input);
+    }
+
+}
